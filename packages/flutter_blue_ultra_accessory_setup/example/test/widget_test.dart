5436f50efc3b9b5192600fce0ddea4ef4122a3ee
Shpaner <tobiasz.witalis@gmail.com>
2026-05-28 17:30:29 +0200

    Remove widget test

--- packages/flutter_blue_ultra_accessory_setup/example/test/widget_test.dart
+++ packages/flutter_blue_ultra_accessory_setup/example/test/widget_test.dart
@@ -1,9 +1,1 @@
-//todo
-void main() {
-  // testWidgets('shows accessory setup example shell', (tester) async {
-  //   await tester.pumpWidget(const AccessorySetupExampleApp());
-  //
-  //   expect(find.text('Accessory SetupKit'), findsOneWidget);
-  //   expect(find.text('Pair an accessory'), findsOneWidget);
-  // });
-}
+void main() {}
