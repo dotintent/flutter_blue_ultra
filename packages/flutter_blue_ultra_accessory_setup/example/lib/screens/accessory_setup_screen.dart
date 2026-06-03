7d2d2884b441c1a1ba2a805044102e2a44d36133
Shpaner <tobiasz.witalis@gmail.com>
2026-06-03 13:51:47 +0200

    Upgrade Flutter

--- packages/flutter_blue_ultra_accessory_setup/example/lib/screens/accessory_setup_screen.dart
+++ packages/flutter_blue_ultra_accessory_setup/example/lib/screens/accessory_setup_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,6 +15,23 @@ class AccessorySetupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // AccessorySetupKit is iOS-only. The plugin uses Objective-C FFI bindings
+    // and will crash on any other platform when constructed.
+    final isIOS = !kIsWeb && Platform.isIOS;
+    if (!isIOS) {
+      return const Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Text(
+              'AccessorySetupKit is only available on iOS.',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      );
+    }
+
     return BlocProvider(
       create: (_) => AccessorySetupCubit()..initialize(),
       child: const _AccessorySetupView(),
